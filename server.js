39e548eaedf8dc9bb308641d3225218774609f1b
Renato Mendes <rnt.mendes11@gmail.com>
2024-05-15 10:34:07 -0300

    agr vai

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-
-require('dotenv').config()
-
+require('dotenv').config();
+const connectToDB = require("./startup/db");
 const app = express();
-
-async function connectToDB() {
-  try {
-    await mongoose.connect(process.env.MONGODB_CONNECT_URI);
-    console.log("Connected to MongoDB Atlas");
-  } catch (error) {
-    console.error("Error connecting to MongoDB Atlas:", error);
-  }
-}
 
 connectToDB();
 
 app.use(
   cors({
     origin: true,
-    methods: "GET,POST,DELETE",
+    methods: "GET,POST,DELETE, PUT",
     preflightContinue: false,
     optionsSuccessStatus: 204
   })
 );
 
+app.use(express.json());
 require("./startup/routes")(app);
-
 const port = process.env.PORT || 8080;
-
 app.listen(port, () => console.log(`Acesse: http://localhost:${port}/`));
-
